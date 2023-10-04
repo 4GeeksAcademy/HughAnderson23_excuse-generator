5c123e5bc71e86e2853458dfefa1b0d3d76a18f3
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-10-04 21:24:26 +0000

    final code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+  console.log("Hello, please generate some excuses by clicking the button.");
+};
+let generateExcuse = () => {
+  let subject = ["My dog", "The cow", "A random student", "That bird"];
+  let verb = ["ate", "threw up", "flew away with", "took"];
+  let object = ["my homework", "my dinner", "my money", "my keys"];
+  let when = [
+    "before the class.",
+    "after I left.",
+    "before I finished.",
+    "during my lunch.",
+    "while I was sleeping."
+  ];
+
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let objectIndex = Math.floor(Math.random() * object.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    subject[subjectIndex] +
+    " " +
+    verb[verbIndex] +
+    " " +
+    object[objectIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
