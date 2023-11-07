6c7640c90ffab8645b79fc2a8e34fce387880dda
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-11-07 02:06:25 +0000

    Edits and added questions.

--- src/app.js
+++ src/app.js
@@ -10,24 +10,30 @@ window.onload = () => {
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#the-excuse").innerHTML = generateExcuse();
   });
-  console.log("Hello, please generate some excuses by clicking the button.");
 };
 let generateExcuse = () => {
   let subject = ["My dog", "The cow", "A random student", "That bird"];
   let verb = ["ate", "threw up", "flew away with", "took"];
   let object = ["my homework", "my dinner", "my money", "my keys"];
   let when = [
-    "before the class.",
-    "after I left.",
-    "before I finished.",
-    "during my lunch.",
-    "while I was sleeping."
+    "before the class",
+    "after I left",
+    "before I finished",
+    "during my lunch time",
+    "while I was sleeping"
+  ];
+  let question = [
+    "what's up with that?",
+    "isn't that wierd?",
+    "don't you find that strange?",
+    ""
   ];
 
   let subjectIndex = Math.floor(Math.random() * subject.length);
   let verbIndex = Math.floor(Math.random() * verb.length);
   let objectIndex = Math.floor(Math.random() * object.length);
   let whenIndex = Math.floor(Math.random() * when.length);
+  let questionIndex = Math.floor(Math.random() * question.length);
 
   return (
     subject[subjectIndex] +
@@ -36,6 +42,8 @@ let generateExcuse = () => {
     " " +
     object[objectIndex] +
     " " +
-    when[whenIndex]
+    when[whenIndex] +
+    ", " +
+    question[questionIndex]
   );
 };
